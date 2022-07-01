b9e2d73cf423995d6dd275fcbe9a2b077ff04b09
jchoi10 <noreply>
2022-06-30 23:03:27 -0400

    add interactive input

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+window.alert("This is an alert! JavaScript is running!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
+//when you want to write a comment
+var playerName = window.prompt("What is your robot's name?")
+//note the lack of quatation marks around playerName
+console.log(playerName)
+
+var name = "your name";
+console.log(name);
